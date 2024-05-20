f1209b6bb3e5464d5331e94795ad3311729c8949
AlexaniaCenteno <alexaniapauth@gmail.com>
2024-05-20 10:05:13 -0600

    Tercer Ejercicio

---
+++ Lista Nombres/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] nombres = { "Itza", "Victor", "Alexania", "Lia", "Norvin", "Nate" };
+Console.WriteLine("Ingrese el nombre a buscar");
+string buscar = Console.ReadLine();
+bool encontrado = nombrelista(nombres, buscar);
+
+if (encontrado)
+{
+    Console.WriteLine("El nombre se encuentra en la lista.");
+    Console.ReadKey();
+}
+else
+{
+    Console.WriteLine("El nombre no se encuentra en la lista.");
+    Console.ReadKey();
+}
+        
+static bool nombrelista(string[] arreglo, string valor)
+{
+    for (int i = 0; i < arreglo.Length; i++)
+    {
+        if (arreglo[i] == valor)
+        {
+            return true;
+        }
+    }
+    return false;
+
+}
